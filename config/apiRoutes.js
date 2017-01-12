d74abfac495113cc2da14dfb809c88e8b4e7b529
savaris <hudhayfajamalkhan@gmail.com>
2017-01-12 14:13:47 +0000

    Api-Routes added for authentications

---
+++ config/apiRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const authentications = require('../controllers/authentications');
+
+router.route('/register')
+  .post(authentications.register);
+router.route('login')
+  .post(authentications.login);
