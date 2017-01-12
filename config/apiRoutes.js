c7ba208581b652f716863aa0a8aef6f753ce0644
savaris <hudhayfajamalkhan@gmail.com>
2017-01-12 15:49:24 +0000

    Working Login/Register

--- config/apiRoutes.js
+++ config/apiRoutes.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const router = express.Router();
+const express  = require('express');
+const router   = express.Router();
 
 const authentications = require('../controllers/authentications');
 const users = require('../controllers/users');
 
 router.route('/register')
   .post(authentications.register);
-router.route('login')
+router.route('/login')
   .post(authentications.login);
 
 router.route('/users')
