719419c72f1b65691694b463289223cc3c33751a
savaris <hudhayfajamalkhan@gmail.com>
2017-01-12 15:29:49 +0000

    Index/app.js integration

---
+++ src/js/app.js
@@ -0,0 +1,141 @@
+const App = App || {};
+
+App.init = function() {
+  this.apiUrl = 'http://localhost:3000/api';
+  this.$main  = $('main');
+  $('.register').on('click', this.register.bind(this));
+  $('.login').on('click', this.login.bind(this));
+  $('.logout').on('click', this.logout.bind(this));
+  $('.usersIndex').on('click', this.usersIndex.bind(this));
+  this.$main.on('submit', 'form', this.handleForm);
+
+  if (this.getToken()) {
+    this.loggedInState();
+  } else {
+    this.loggedOutState();
+  }
+};
+
+App.loggedInState = function(){
+  $('.loggedIn').show();
+  $('.loggedOut').hide();
+  this.usersIndex();
+};
+
+App.loggedOutState = function(){
+  $('.loggedIn').hide();
+  $('.loggedOut').show();
+  this.register();
+};
+
+App.register = function(e){
+  if (e) e.preventDefault();
+  this.$main.html(`
+    <h2>Register</h2>
+    <form method="post" action="/register">
+      <div class="form-group">
+        <input class="form-control" type="text" name="user[username]" placeholder="Username">
+      </div>
+      <div class="form-group">
+        <input class="form-control" type="email" name="user[email]" placeholder="Email">
+      </div>
+      <div class="form-group">
+        <input class="form-control" type="password" name="user[password]" placeholder="Password">
+      </div>
+      <div class="form-group">
+        <input class="form-control" type="password" name="user[passwordConfirmation]" placeholder="Password Confirmation">
+      </div>
+      <input class="btn btn-primary" type="submit" value="Register">
+    </form>
+  `);
+};
+
+App.login = function(e) {
+  e.preventDefault();
+  this.$main.html(`
+    <h2>Login</h2>
+    <form method="post" action="/login">
+      <div class="form-group">
+        <input class="form-control" type="email" name="email" placeholder="Email">
+      </div>
+      <div class="form-group">
+        <input class="form-control" type="password" name="password" placeholder="Password">
+      </div>
+      <input class="btn btn-primary" type="submit" value="Login">
+    </form>
+  `);
+};
+
+App.logout = function(e){
+  e.preventDefault();
+  this.removeToken();
+  this.loggedOutState();
+};
+
+App.usersIndex = function(e) {
+  if (e) e.preventDefault();
+  const url = `${this.apiUrl}/users`;
+
+  return this.ajaxRequest(url, 'get', null, data => {
+    this.$main.html(`
+      <div class="card-deck-wrapper">
+        <div class="card-deck">
+        </div>
+      </div>
+    `);
+    const $container = this.$main.find('.card-deck');
+    $.each(data.users, (i, user) => {
+      $container.append(`
+        <div class="card col-md-4">
+         <div class="card-block">
+           <h4 class="card-title">${user.username}</h4>
+          </div>
+       </div>`);
+    });
+  });
+};
+
+App.handleForm = function(e){
+  e.preventDefault();
+
+  const url    = `${App.apiUrl}${$(this).attr('action')}`;
+  const method = $(this).attr('method');
+  const data   = $(this).serialize();
+
+  return App.ajaxRequest(url, method, data, data => {
+    if (data.token) App.setToken(data.token);
+    App.loggedInState();
+  });
+};
+
+App.ajaxRequest = function(url, method, data, callback){
+  return $.ajax({
+    url,
+    method,
+    data,
+    beforeSend: this.setRequestHeader.bind(this)
+  })
+  .done(callback)
+  .fail(data => {
+    console.log(data);
+  });
+};
+
+App.setRequestHeader = function(xhr) {
+  return xhr.setRequestHeader('Authorization', `Bearer ${this.getToken()}`);
+};
+
+App.setToken = function(token){
+  return window.localStorage.setItem('token', token);
+};
+
+App.getToken = function(){
+  return window.localStorage.getItem('token');
+};
+
+App.removeToken = function(){
+  return window.localStorage.clear();
+};
+
+
+$(App.init.bind(App));
