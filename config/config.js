3f86778c94bbb683566aa9937ddae8c440d0d098
savaris <hudhayfajamalkhan@gmail.com>
2017-01-18 10:08:41 +0000

    Adding process.env

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: 'mongodb://localhost:27017/wdi_project_2_web_cams',
+  db: process.env.MONGODB_URI || 'mongodb://localhost:27017/wdi_project_2_web_cams',
   secret: process.env.SECRET || 'secret'
 };
